0bb782a3e55c3b496c5729fec5b30e4d0363e6fa
Madison McCardle <madison.mccardle@powerhrg.com>
2020-07-23 13:49:49 -0400

    add users/ display them

--- shoeproject-backend/app/controllers/users_controller.rb
+++ shoeproject-backend/app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def index
     users = User.all
-    render json: users.to_json(
+    render json: users.to_json
       # include: { :shoes
       # }
-    )
+
   end
 
   def create
--- shoeproject-frontend/src/index.js
+++ shoeproject-frontend/src/index.js
@@ -3,6 +3,7 @@ let addUser = false;
 let showCards = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+
   const addBtn = document.querySelector("#new-shoe-btn"); // botton that shows the form for adding new shoe
   const shoeUserSection = document.querySelector("#new-or-old-user"); // class container has the form for add new shoe
   addBtn.addEventListener("click", () => {
@@ -25,27 +26,78 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   })
 
+  document.querySelector(".add-user-form").addEventListener("submit", (submitevent) => {
+    submitevent.preventDefault()
+    const nameEl = document.querySelector("#name-input") //added id to index.html
+    const imageEl = document.querySelector("#image-input") //added id to index.html
+
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        "name": nameEl.value,
+        "image": imageEl.value,
+      })
+    }).then(resp => { return resp.json()
+    }).then(user => {
+      cardContainer.style.display = "block"
+      addCard(user)
+      nameEl.value = "" //this will revert the text box to blank once submitted
+      imageEl.value = ""
+    })
+  })
+
   const previousUserBtn = document.querySelector("#previous-user")
   const cardContainer = document.querySelector("#shoe-collection")
-  cardContainer.innerHTML =
+  cardContainer.innerHTML = ""
   previousUserBtn.addEventListener("click", () => {
     showCards = !showCards
     if (showCards) {
       cardContainer.style.display = "block"
+      cardContainer.innerHTML = ""
+      loadUsers()
     } else {
       cardContainer.style.display = "none"
     }
   })
-
-  const submitNewUserBtn = document.querySelector("#submit-new-user")
-  submitNewUserBtn.addEventListener("submit", () => {
-    fetch("localhost:3000/user")
-
-    cardContainer.style.display = "block"
-    cardContainer.innerHTML = "hello"
-  })
-
-
-
 })
+
+  function loadUsers() {
+    fetch("http://localhost:3000/users")
+    .then(resp => resp.json())
+    .then(allUsers => {
+      allUsers.forEach(user => addCard(user))
+      //allUsers.included.forEach(shoe => listShoe(shoe))
+
+    })
+  }
+
+  function addCard(user) {
+    const card = document.createElement('div');
+    const shoeList = document.createElement('ul');
+    const addShoeButton = document.createElement('button');
+    const cardContainer = document.querySelector("#shoe-collection")
+
+    card.setAttribute("data-id", user.id)
+    shoeList.id = `shoe-list-${user.id}`
+    shoeList.className = "shoe-list"
+    addShoeButton.id = 'add-button'
+    addShoeButton.innerText = 'Add Shoe'
+    card.classList.add("card")
+
+    userName = document.createElement('h1')
+    userName.innerHTML = user.name
+    userImage = document.createElement('img')
+    userImage.src = user.image_url
+
+    cardContainer.prepend(card)
+    card.append(userName)
+    card.append(userImage)
+    card.append(addShoeButton)
+    card.append(shoeList)
+
+}
 
