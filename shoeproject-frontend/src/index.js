c014fa2d13411a73045342e114a88bfb8fdbe2af
Madison McCardle <madison.mccardle@powerhrg.com>
2020-07-23 09:11:29 -0400

    reconfigured intial set up. added new userform

--- shoeproject-frontend/src/index.js
+++ shoeproject-frontend/src/index.js
@@ -1,15 +1,30 @@
-let addShoe = false;
-
-ocument.addEventListener("DOMContentLoaded", () => {
+let createShoe = false;
+let addUser = false;
+document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-shoe-btn"); // botton that shows the form for adding new shoe
-  const shoeFormContainer = document.querySelector(".container"); // class container has the form for add new shoe
+  const shoeUserSection = document.querySelector("#new-or-old-user"); // class container has the form for add new shoe
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
-    addShoe = !addShoe;
-    if (addShoe) {
-      shoeFormContainer.style.display = "block";
+    createShoe = !createShoe;
+    if (createShoe) {
+      shoeUserSection.style.display = "block";
     } else {
-      shoeFormContainer.style.display = "none";
+      shoeUserSection.style.display = "none";
     }
   });
+  const newUserBtn = document.querySelector("#new-user")
+  const newUserContainer = document.querySelector("#new-user-form-container")
+  newUserBtn.addEventListener("click", () => {
+    addUser = !addUser
+    if (addUser) {
+      newUserContainer.style.display = "block"
+    } else {
+      newUserContainer.style.display = "none"
+    }
+  })
+
+
+
+
 })
+
