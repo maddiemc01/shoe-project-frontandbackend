6343f1f91bfe06089489e8dd4e89dec4fd340aaf
Madison McCardle <madison.mccardle@powerhrg.com>
2020-07-29 10:59:33 -0400

    refactor toggle form

--- shoeproject-backend/db/seeds.rb
+++ shoeproject-backend/db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: "example user", image_url: "https://www.google.com/search?q=cinderella&sxsrf=ALeKk00tF6tNTWnPrp-YjxLl_fc7PTEgpg:1595514344333&source=lnms&tbm=isch&sa=X&ved=2ahUKEwizi-mNyuPqAhWzZDUKHUe0CNAQ_AUoAXoECCoQAw&biw=1673&bih=902#imgrc=gVETMp8GCn4GGM")
-User.create(name: "cinderella", image_url: "https://www.google.com/search?q=cinderella&sxsrf=ALeKk00tF6tNTWnPrp-YjxLl_fc7PTEgpg:1595514344333&source=lnms&tbm=isch&sa=X&ved=2ahUKEwizi-mNyuPqAhWzZDUKHUe0CNAQ_AUoAXoECCoQAw&biw=1673&bih=902#imgrc=gVETMp8GCn4GGM")
+User.create(name: 'example user', image_url: 'https://www.google.com/search?q=cinderella&sxsrf=ALeKk00tF6tNTWnPrp-YjxLl_fc7PTEgpg:1595514344333&source=lnms&tbm=isch&sa=X&ved=2ahUKEwizi-mNyuPqAhWzZDUKHUe0CNAQ_AUoAXoECCoQAw&biw=1673&bih=902#imgrc=gVETMp8GCn4GGM')
+User.create(name: 'cinderella', image_url: 'https://www.google.com/search?q=cinderella&sxsrf=ALeKk00tF6tNTWnPrp-YjxLl_fc7PTEgpg:1595514344333&source=lnms&tbm=isch&sa=X&ved=2ahUKEwizi-mNyuPqAhWzZDUKHUe0CNAQ_AUoAXoECCoQAw&biw=1673&bih=902#imgrc=gVETMp8GCn4GGM')
 
-Shoe.create(name: "glass slipper", size: "9", style: "stilletto", heel_height: "4 inches", color: "ff000", user_id: 2)
-Shoe.create(name: "example shoe 1", size: "9", style: "stilletto", heel_height: "4 inches", color: "ff000", user_id: 1)
+Shoe.create(name: 'glass slipper', size: '9', style: 'stilletto', heel_height: '4 inches', color: 'ff000', user_id: 2)
+Shoe.create(name: 'example shoe 1', size: '9', style: 'stilletto', heel_height: '4 inches', color: 'ff000', user_id: 1)
